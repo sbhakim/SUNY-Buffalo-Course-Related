fcd1b05b4db28638b18899c96f69fe76b1ed7558
sbhakim <sbhakim@kth.se>
2022-11-27 11:39:32 -0500

    fixed-bubbelsort

--- Algorithm-DStuct/sorting/bubblesort.c
+++ Algorithm-DStuct/sorting/bubblesort.c
@@ -2,45 +2,58 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#define LENGTH 10
 
-#define LENGTH  20
-
-uint8_t fNumbers[LENGTH];
-uint8_t *fNumRefs[LENGTH];
+uint8_t numbers[LENGTH];
+uint8_t *num_ptr[LENGTH];
+void swap (uint8_t** pointer1, uint8_t** pointer2);
+void bsort();
+void init();
 
 void init(){
     srand(time(NULL));
-    printf("Numbers: \n");
+    printf ("Numbers: \n");
     for (size_t i = 0; i < LENGTH; i++){
-        fNumbers[i] = rand() %256;
-        fNumRefs[i] = &fNumbers[i];
-        printf("%hhu   ", fNumbers[i]);
+        numbers[i] = rand()%256;
+        num_ptr[i] = &numbers[i];
+        printf ("%hhu   ", *num_ptr[i]);
     }
+    printf ("\n");
 }
 
-void swap(uint8_t** pointer1, uint8_t** pointer2){
+ void bsort(){
+        for (size_t i=0; i<LENGTH; i++){
+            for (size_t j=0; j<LENGTH-1; j++){
+                if(*num_ptr[j] > *num_ptr[j+1]){
+                    swap(&num_ptr[j], &num_ptr[j+1]);
+                }
+            }
+        }
+}
+
+void printArray(){
+    printf("The sorted array is: \n");
+    for (size_t i =0; i < LENGTH; i++){
+        printf ("%hhu   ", *num_ptr[i]);
+    }
+    printf ("\n");
+}
+
+
+void swap (uint8_t** pointer1, uint8_t** pointer2){
     *pointer1 = (uintptr_t) (*pointer1)^(uintptr_t) (*pointer2);
     *pointer2 = (uintptr_t) (*pointer1)^(uintptr_t) (*pointer2);
     *pointer1 = (uintptr_t) (*pointer1)^(uintptr_t) (*pointer2);
 }
 
-void bsort(){
-    for(size_t i = 0; i < LENGTH; i++){
-        for (size_t j = 0; j < LENGTH-1 -i; j++){
-            if (*fNumRefs[j] > *fNumRefs[j +1 ]){
-                swap(&fNumRefs[j], &fNumRefs[j+1]);
-            }
-        }
-    }
-}
 
-
-int main() {
+int main(){
+    double time_spent = 0.0;
+    clock_t begin = clock();
     init();
     bsort();
-    printf("\nSorted : \n");
-    for (size_t i = 0; i < LENGTH; i++)
-        printf("%hhu   ", *fNumRefs[i]);
-    printf("\n");
+    printArray();
+    clock_t end= clock();
+    printf("Number of clock cycles to compute : %hhu\n", end - begin);
     return 0;
-
+}
