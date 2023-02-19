fe35fedc4f16bc28511d437c3f6c97b9b0c5c674
sbhakim <sbhakim@kth.se>
2023-02-19 18:57:49 -0500

    add detailed comments

--- MIPS-Programs/recursive-factorial.s
+++ MIPS-Programs/recursive-factorial.s
@@ -14,7 +14,7 @@ main:
 
         li $v0, 5 
         syscall 
-        add $a0, $v0, $zero  
+        add $a0, $v0, $zero   # moving the input value to $a0
 
 
         # Move factorial result into temp $t0 
@@ -38,20 +38,24 @@ end_prog:
         syscall 
 
 fact: 
-        addi $sp, $sp, -4     # decrement stack pointer, stack growing 
-        sw $ra, 0($sp)        # push the value of $ra on the stack 
-        addi $sp, $sp, -4     # decrement stack pointer, stack growing
-        sw $a0, 0($sp)
+        addi $sp, $sp, -4     # increment stack pointer, stack growing, stack TOP changed
+        sw $ra, 0($sp)        # → value of $ra is stored in stack TOP, PUSH to stack
+        addi $sp, $sp, -4     # increment stack pointer, stack growing, stack TOP changed
+        sw $a0, 0($sp)        # → value of $a0 is stored in stack TOP, PUSH to stack
 
-        slti $t0, $a0, 1      # $t0=1, if $a0 < 1 
-        beq $t0, $zero, L1 
+        slti $t0, $a0, 1      # $t0=1, if $a0 <= 1. most of the times $t0 = 0 
+        beq $t0, $zero, L1    # This willl occur (n-1) times
 
+        # after jumping (n-1) times to L1, only for once the following will be executed
         addi $v0, $zero, 1 
 
-        lw $a0, 0($sp)        # pop the value of $a0 from the stack 
-        addi $sp, $sp, 4
+        lw $a0, 0($sp)        # ← POP the value of $a0 from the stack 
+        addi $sp, $sp, 4      # decrement the stack pointer, stack shrink  
+                              # after POP stack shrinks
+
         lw $ra, 0($sp)
-        addi $sp, $sp, 4 
+        addi $sp, $sp, 4      # decrement the stack pointer, stack shrink  
+
 
 
         # if condition fullfils may not jump to L1 
@@ -60,17 +64,17 @@ fact:
 
 
 L1: 
-        addi $a0, $a0, -1    # decrement already performed (n - 1)
+        addi $a0, $a0, -1    #  (n - 1) operation
         jal fact             # jal - jump and link (unconditional jump) 
 
 
-        lw $a0, 0($sp)       # ← pop the value to $a0 from the stack  
-        addi $sp, $sp, 4     # increment the stack pointer, stack shrink  
+        lw $a0, 0($sp)       # ← POP the value to $a0 from the stack  
+        addi $sp, $sp, 4     # decrement the stack pointer, stack shrink  
 
-        lw $ra, 0($sp)       # ← pop the value from the stack to $ra 
-        addi $sp, $sp, 4     # increment the stack pointer, stack shrink 
+        lw $ra, 0($sp)       # ← POP the value from the stack to $ra 
+        addi $sp, $sp, 4     # decrement the stack pointer, stack shrink 
 
-        mult $a0, $v0        # n*fact(n-1)
+        mult $a0, $v0        # n*fact(n-1), $a0 and $v0 value unchanged, stored in HI, LO
         mflo $v0             # moves the lower 32-bit of the mult result to $v0 
 
 
