6dd20b3c6859b48fdeff25f8daf881a057ae1df3
sbhakim <sbhakim@kth.se>
2023-03-07 12:16:07 -0500

    RSA python code modified

--- CS-565-CompSecurity/OpenSSL-RSA/rsa_encrypt.py
+++ CS-565-CompSecurity/OpenSSL-RSA/rsa_encrypt.py
@@ -1,56 +1,75 @@
 #! /usr/bin/python3
 
 
-
-
-
 import time
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-start_time = time.time()
+class RSAEncryptor:
+    def __init__(self):
+        # Generate RSA key pair
+        self.key = RSA.generate(2048)
+        
+        # Save private key to file
+        with open('private_key.pem', 'wb') as f:
+            f.write(self.key.export_key('PEM'))
+        
+        # Save public key to file
+        with open('public_key.pem', 'wb') as f:
+            f.write(self.key.publickey().export_key('PEM'))
+        
+        # Load public key from file
+        with open('public_key.pem', 'rb') as f:
+            self.public_key = RSA.import_key(f.read())
+    
+    def encrypt(self, message):
+        start_time = time.time()
+        # Encrypt message using public key
+        cipher = PKCS1_OAEP.new(self.public_key)
+        ciphertext = cipher.encrypt(message)
+        end_time = time.time()
+        
+        encryption_time = end_time - start_time
+        encryption_speed = encryption_time / len(message) * 1000
 
-# Generate RSA key pair
-key = RSA.generate(2048)
+        print('Encryption time: {:.2f} ms'.format(encryption_time * 1000))
+        print('Encryption speed per byte: {:.5f} ms/byte'.format(encryption_speed))
 
-end_time = time.time()
-key_gen_time = (end_time - start_time) * 1000  # Convert to milliseconds
+        
+        return ciphertext
+    
+    def decrypt(self, ciphertext):
+        # Load private key from file
+        with open('private_key.pem', 'rb') as f:
+            private_key = RSA.import_key(f.read())
+        
+        start_time = time.time()
+        # Decrypt ciphertext using private key
+        cipher = PKCS1_OAEP.new(private_key)
+        plaintext = cipher.decrypt(ciphertext) 
+        end_time = time.time()
+        
+        decryption_time = end_time - start_time
+        decryption_speed = decryption_time / len(plaintext) * 1000
 
-# Save private key to file
-with open('private_key.pem', 'wb') as f:
-    f.write(key.export_key('PEM'))
+        print('Decryption time: {:.2f} ms'.format(decryption_time * 1000))
+        print('Decryption speed per byte: {:.5f} ms/byte'.format(decryption_speed))
+        
+      
+        return plaintext
 
-# Save public key to file
-with open('public_key.pem', 'wb') as f:
-    f.write(key.publickey().export_key('PEM'))
+if __name__ == '__main__':
+    # Read file contents
+    with open('short_file.txt', 'rb') as f:
+        message = f.read()
+        
+    encryptor = RSAEncryptor()
+    ciphertext = encryptor.encrypt(message)
+    plaintext = encryptor.decrypt(ciphertext)
+    
+    # Print results
+    print('Original message:', message)
 
-# Load public key from file
-with open('public_key.pem', 'rb') as f:
-    public_key = RSA.import_key(f.read())
 
-# Read file contents
-with open('short_file.txt', 'rb') as f:
-    message = f.read()
-
-# Encrypt message using public key
-cipher = PKCS1_OAEP.new(public_key)
-ciphertext = cipher.encrypt(message)
-
-# Load private key from file
-with open('private_key.pem', 'rb') as f:
-    private_key = RSA.import_key(f.read())
-
-# Decrypt ciphertext using private key
-cipher = PKCS1_OAEP.new(private_key)
-plaintext = cipher.decrypt(ciphertext)
-
-# Print results
-print('Original message:', message)
-print(" " * 10)
-print('Encrypted message:', ciphertext)
-print(" " * 10)
-print('Decrypted message:', plaintext)
-print(" " * 10)
-print('Key generation time (ms):', key_gen_time)
 
 
